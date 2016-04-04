067b521c50374cccaae8d045cf0dfaabdfd49020
Aegir user <aegir@ec2-52-28-207-90.eu-central-1.compute.amazonaws.com>
2016-04-04 22:04:31 +0200

    Remove varnish and memcached

--- sites/media-youtube-upload.com/files/advagg_js/js__hlKmHb8qCLIXrrq5jJL4cVWYPBDRA6X_YnOAyrGMBWU__zA3mNMz2TKC0rB1hvFKoJK5BT_0IVCCKlE_v3R54N1c__rhUWvSexRuCQqyIkzin7eQ7T6Mxrsasz32WqmCICDZc.js
+++
@@ -1,6 +0,0 @@
-/* Source and licensing information for the line(s) below can be found at http://media-youtube-upload.com/sites/all/themes/bootstrap/js/misc/states.js. */
-(function($){$(document).unbind('state:disabled');$(document).bind('state:disabled',function(e){if(e.trigger)$(e.target).attr('disabled',e.value).closest('.form-item, .form-submit, .form-wrapper').toggleClass('form-disabled',e.value).find(':input').attr('disabled',e.value)})})(jQuery);;
-/* Source and licensing information for the above line(s) can be found at http://media-youtube-upload.com/sites/all/themes/bootstrap/js/misc/states.js. */
-/* Source and licensing information for the line(s) below can be found at http://media-youtube-upload.com/sites/all/themes/bootstrap/js/misc/tabledrag.js. */
-(function($){Drupal.tableDrag.prototype.row.prototype.markChanged=function(){var $cell=$('td:first',this.element),$target=$($cell.find('.file-size').get(0)||$cell.find('.file').get(0)||$cell.find('.tabledrag-handle').get(0));if(!$cell.find('.tabledrag-changed').length)$target.after(' '+Drupal.theme('tableDragChangedMarker')+' ')}})(jQuery);;
-/* Source and licensing information for the above line(s) can be found at http://media-youtube-upload.com/sites/all/themes/bootstrap/js/misc/tabledrag.js. */
